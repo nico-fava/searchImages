9b8261ed6eb699f181412fff36675d47739323a9
Nikdev <favarettonicolo9@gmail.com>
2025-02-12 16:04:42 +0100

    Stylish input field with a focus effect Animated search button Smooth hover effects

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,24 +2,30 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchImages } from '../store/imageSlice'
 import { AppDispatch } from '../store/store'
+import '../styles/search.scss'
 
 const SearchBar = () => {
   const [query, setQuery] = useState<string>('')
   const dispatch = useDispatch<AppDispatch>()
 
   const handleSearch = () => {
-    dispatch(fetchImages(query))
+    if (query.trim()) {
+      dispatch(fetchImages(query))
+    }
   }
 
   return (
-    <div>
+    <div className="search-bar">
       <input
         type="text"
         placeholder="Search images..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
       />
-      <button onClick={handleSearch}>Search</button>
+      <button onClick={handleSearch}>
+        <span>Search</span>
+      </button>
     </div>
   )
 }
